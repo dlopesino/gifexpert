e87638153940ae674a254be08eca6f995261fbb9
daniellopesino <daniel.lopesino@traspacar.com>
2020-07-26 12:06:57 +0200

    docs agregado

---
+++ docs/precache-manifest.c3827a19abf50759f93a9d5c6d54e135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15801636150e21f8e5b16b093080b63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d5d49c354b3aae6df95",
+    "url": "/static/css/main.0112abea.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5d49c354b3aae6df95",
+    "url": "/static/js/main.a8704d1b.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
